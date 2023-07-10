8f2de94c03ccfa9296452c23c066c1f76724561d
Gabriel Lopes Dallafávera <97198030+GabrielDalla1@users.noreply.github.com>
2023-07-10 11:35:50 -0300

    Add files via upload

---
+++ Consult Companies/consultacloud.php
@@ -0,0 +1,52 @@
+<?php
+
+$url = 'https://cloud.liguemaistelecom.com.br/pabx/api.php';
+
+$valor = 220; // Inicializa a variável com valor 0
+
+
+
+$data = array(
+    'autenticacao' => array(
+        'usuario' => 'TOKEN_NAME',
+        'token' => 'TOKEN_HASH'
+    ),
+    'acao' => 'listar_clientes',
+    'cliente_id' => "",
+    'nome' => "",
+    'pos_registro_inicial' => $valor,
+);
+
+
+$data_string = json_encode($data);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string)
+));
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+$result = curl_exec($ch);
+
+if ($result === false) {
+    // Handle error
+    echo "Failed to call the API: " . curl_error($ch);
+} else {
+    // Process the API response
+    $response = json_decode($result, true);
+    // Access the returned company information
+    // Example: $companyName = $response['empresas'][0]['nome'];
+    
+}
+   $responsee = json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_OBJECT_AS_ARRAY );
+    echo "<pre>";
+         print_r($responsee);
+    echo "</pre>";
+curl_close($ch);
+
+?>
